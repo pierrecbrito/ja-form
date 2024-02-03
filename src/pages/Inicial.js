94146ed1cab4b4e32a8aa93f353bcbe6961a6c27
Pierre Brito <carlospierre07@gmail.com>
2024-02-03 10:27:20 -0300

    Tela inicial

---
+++ src/pages/Inicial.js
@@ -0,0 +1,14 @@
+import logo from '../JA.png';
+import '../css/Inicial.css';
+
+function Inicial() {
+  return (
+    <div className="App">
+      <main className="App-fundo">
+        <img src={logo} className="App-logo" alt="logo da JA" />
+      </main>
+    </div>
+  );
+}
+
+export default Inicial;
